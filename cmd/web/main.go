f7a73fd4b4faa0beea8621102b4055865ddebd5c
Cesar <ccordero.007@gmail.com>
2025-07-22 09:47:24 -0600

    Isolating the application routes

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -39,23 +39,23 @@ func main() {
 
 	// Use the http.NewServerMux() function to initialize a new servemux, then
 	// register the home function as the handler for the "/" URL pattern.
-	mux := http.NewServeMux()
+	// mux := http.NewServeMux()
 
 	// Create a file server which serves files out of the "./ui/static" directory.
 	// Note that path given to the http.Dir function is relative to the project
 	// directy root
-	fileServer := http.FileServer(http.Dir("./ui/static"))
+	// fileServer := http.FileServer(http.Dir("./ui/static"))
 
 	// Use the mux.handle() function to register the file server as the handler for
 	// all URL paths that start with "/static". For matching paths, we strip the
 	// "/static" prefix before the request reaches the file server.
-	mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
+	// mux.Handle("GET /static/", http.StripPrefix("/static", fileServer))
 
-	mux.HandleFunc("GET /{$}", app.home)                      //Restrict this route to exact matches on / only
-	mux.HandleFunc("GET /snippet/view/{id}", app.snippetView) // Add the (id) wildcard segment
-	mux.HandleFunc("GET /snippet/create", app.snippetCreate)
+	// mux.HandleFunc("GET /{$}", app.home)                      //Restrict this route to exact matches on / only
+	// mux.HandleFunc("GET /snippet/view/{id}", app.snippetView) // Add the (id) wildcard segment
+	// mux.HandleFunc("GET /snippet/create", app.snippetCreate)
 	// Create the new route, which is restricted to POST request only,
-	mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
+	// mux.HandleFunc("POST /snippet/create", app.snippetCreatePost)
 
 	// Print a log message to say that the server is starting
 	// log.Print("Starting server on :4000")
@@ -72,7 +72,7 @@ func main() {
 	// program. Note that any error returned by http.ListenAndServe() is always
 	// non-nil
 	// err := http.ListenAndServe(":4000", mux)
-	err := http.ListenAndServe(*addr, mux)
+	err := http.ListenAndServe(*addr, app.routes())
 	// log.Fatal(err)
 	// And we also use the Error() method to log any error message returned by
 	// http.ListenAndServe() at Error severity (with no additional attributes)
